37b105039216df0a85ca7fe474988e7f40d3791d
YeisonGaona <yfgaona@ucundinamarca.edu.co>
2020-06-23 12:51:07 -0500

    implementacion de servicios new relic

--- nerdlets/my-awesome-nerdpack-nerdlet/index.js
+++ nerdlets/my-awesome-nerdpack-nerdlet/index.js
@@ -3,12 +3,62 @@ import React from 'react';
 import './styles.scss';
 
 export default class MyAwesomeNerdpackNerdletNerdlet extends React.Component {
+
     constructor() {
         super();
         this.state = {
             contador: 0,
-            estado: "#5da662"
+            estado: "#5da662",
+            value: ''
         }
+    }
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    }
+
+    metodoGet() {
+        if (this.state.value !== '') {
+            var nrql = 'SELECT%20uniqueCount(session)%20FROM%20PageView%20SINCE%201%20day%20AGO';
+            var url = `https://insights-api.newrelic.com/v1/accounts/2771470/query?nrql=${nrql}`;
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Query-Key': 'NRIQ-pcEXAVttcDUc6b2QMnicXY0TzYg4a1T9'
+                }
+            }).then(res => {
+                res.json()
+            }).catch(error => {
+                console.error('error', error)
+            }).then(response => {
+                console.log('response', response)
+                // this.setState({value:response.results.textCustom})
+            });
+        }
+    }
+
+
+    metodoPost() {
+        var url = 'https://insights-collector.newrelic.com/v1/accounts/2771470/events/';
+        var data = {
+            "eventType": "textCustom",
+            "texto": this.state.value
+        };
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Encoding': 'application/gzip',
+                'X-Insert-Key': 'NRII-NobwI4PstKZ6lJ16lfOV2SpKZI8k5ZBj',
+            }
+        }).then(res => {
+            res.json()
+        }).catch(error => console.error('Error:', error))
+            .then(response => {
+                this.metodoGet();
+            });
     }
 
     ejecutar() {
@@ -30,6 +80,7 @@ export default class MyAwesomeNerdpackNerdletNerdlet extends React.Component {
                     this.setState({ estado: '#5da662' });
                     break;
             }
+            this.metodoPost();
             this.setState({ contador: 0 });
         }
     }
@@ -47,7 +98,7 @@ export default class MyAwesomeNerdpackNerdletNerdlet extends React.Component {
                     <div className='parte-superior' style={{ background: this.state.estado }}>
                         <br />
                         <br />
-                        <input placeholder='Message to Update' style={txtfield} ></input>
+                        <input placeholder='Message to Update' value={this.state.value} onChange={this.handleChange} style={txtfield} ></input>
                     </div>
                 </div >
             </div>
@@ -58,7 +109,7 @@ export default class MyAwesomeNerdpackNerdletNerdlet extends React.Component {
 
 const txtfield = {
     height: '20px',
-     width: '180px',
+    width: '180px',
     fontSize: '15px',
     border: '2px solid rgb(199, 189, 189)',
     padding: '20px',
